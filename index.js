39ba4eb1514ec828038dfccf4950b709339d22c9
parisdolfman <32081304+parisdolfman@users.noreply.github.com>
2019-01-03 14:33:41 -0500

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ return null // if we haven't done this then return null
 
 1) current var represents the current array we are on to keep track
 2) next keeps track of the elements we have yet to go through
-3) the while loop only executes if there is an element in current and if undefined we'll exit the loop
+3) the while loop only executes if there is an element in current and if undefined we will exit the loop
 4) if (criteriaFn(current)) means if current equals to the criteria then we retun it and exit the function
 5) if variable current is equal to an array iterate through it's elements with a for loop and push all of them onto next
 6) after pushing any children of current onto next - if there are any children - we make the first element of next the new current to be the next to be passed to the while loop and if we haven't done this then return null 

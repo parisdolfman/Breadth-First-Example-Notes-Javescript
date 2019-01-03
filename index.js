080e53a6bf75d792e6b4fdb2652dc47b6f01a89c
parisdolfman <32081304+parisdolfman@users.noreply.github.com>
2019-01-03 14:33:29 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function find(array, criteriaFn) {  
+function find(array, criteriaFn) {  // two parameters for array to be searched through and the criteria to find
  let current = array   // current var represents the current array we are on to keep track
  let next = []    // next ver keeps track of the elements we have yet to go through
  while (current) {    // the while loop only executes if there is an element in current and if undefined we'll exit the loop
